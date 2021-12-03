a9f058bbbc1f08084766d98c015e9d729c0857af
Charles <crg0905@gmail.com>
2021-12-02 16:26:00 -0800

    first commit

---
+++ screens/CategoryMealScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const CategoryMealScreen = (props) => {
+  return (
+    <View>
+      <Text>The Category Meal Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CategoryMealScreen;
---
+++ screens/MealDetailsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MealDetailsScreen = (props) => {
+  return (
+    <View>
+      <Text>The Meal MealDetailsScreen Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default MealDetailsScreen;
